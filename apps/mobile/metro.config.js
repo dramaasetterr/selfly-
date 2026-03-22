da625e18580ae2282751f67e0d4f6aceb4e1d2b2
John <dramaasetterr@live.com>
2026-03-22 15:56:11 -0400

    Fix metro watcher warning for EAS

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
-
 const config = getDefaultConfig(__dirname);
-
+// Suppress EAS build warning
+if (config.watcher) {
+  delete config.watcher.unstable_workerThreads;
+}
 module.exports = config;
